b3669483766447e32a88edddccd9b9140864749e
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-11-13 23:07:53 +0300

    testimony - 2

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -2,24 +2,32 @@ import React from 'react';
 import styled from "styled-components";
 import clientImg from '../../assets/images/clientImg.webp'
 import {WorkProgress} from "../workProgress/WorkProgress";
+import {FlexWrapper} from "../FlexWrapper";
 
 
 export const Slider = () => {
     return (
         <StyledSlider>
-            <Slide>
-                <Text>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a,
-                    mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut
-                    interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class
-                    aptent taciti sociosqu ad litora torquent
-                </Text>
 
-                <Img src={clientImg} alt='clientImg'/>
+            <FlexWrapper>
+                <Slide>
+                    <Text>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a,
+                        mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut
+                        interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class
+                        aptent taciti sociosqu ad litora torquent
+                    </Text>
 
-                <WorkProgress/>
-            </Slide>
+                    <Img src={clientImg} alt='clientImg'/>
+                    <Pagination>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </Pagination>
 
+                    <WorkProgress/>
+                </Slide>
+            </FlexWrapper>
 
         </StyledSlider>
     );
@@ -28,10 +36,13 @@ export const Slider = () => {
 const StyledSlider = styled.div`
     max-width: 1200px;
     border: 1px solid red;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
 
 const Slide = styled.div`
-    
+
 `
 
 const Text = styled.div`
@@ -46,6 +57,16 @@ const Img = styled.img`
     width: 389px;
     height: 391px;
     opacity: 80%;
+`
+
+const Pagination = styled.span`
+    span {
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        margin: 5px;
+        background-color: azure;
+    }
 `
 
 
